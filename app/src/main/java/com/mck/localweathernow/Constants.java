60587cc02bebdff220b08e6ec09f747bc330f24f
Michael King <michaeldigit@gmail.com>
2016-06-27 10:56:19 -0700

    Include location fragment and place-services-location
    pulled over location helper fragment from base app and included play-services-location to be able to access GoogleClientApi for location services.

--- app/src/main/java/com/mck/localweathernow/Constants.java
+++ app/src/main/java/com/mck/localweathernow/Constants.java
@@ -1,8 +1,12 @@
 package com.mck.localweathernow;
 
 /**
+ * Constants used through out the app are stored here for easy updates
+ * and consistency.
  * Created by Michael on 6/25/2016.
  */
 public class Constants {
     public static final String API_ID = PrivateConstants.API_ID;
+    public static long locationReqInterval = 4000;
+    public static long fastestLocationReqInterval = 1000;
 }
---
+++ app/src/main/java/com/mck/localweathernow/LocationFragment.java
@@ -0,0 +1,209 @@
+package com.mck.localweathernow;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+/**
+ * A simple {@link Fragment} subclass used to handle getting the users
+ * location.
+ */
+public class LocationFragment extends Fragment implements LocationListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private LocationFragmentListener mLocationFragmentListener;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private static final int PERMISSIONS_ACCESS_LOCATION_ID = 23;
+    private static final int REQUEST_CHECK_SETTINGS_ID = 72;
+    private static final int CONN_FAIL_RESLN_RSLT_ID = 43;
+
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment LocationFragment.
+     */
+    public static LocationFragment newInstance() {
+        return new LocationFragment();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof LocationFragmentListener) {
+            mLocationFragmentListener = (LocationFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement LocationFragmentListener");
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // create the googleApiClient for access to google services.
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mGoogleApiClient != null && mGoogleApiClient.isConnected() ){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null &&
+                (mGoogleApiClient.isConnecting() || mGoogleApiClient.isConnected())) {
+            mGoogleApiClient.disconnect();
+
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mLocationFragmentListener = null;
+    }
+
+    // google api client is connected
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // create location request instance
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(Constants.locationReqInterval);
+        mLocationRequest.setFastestInterval(Constants.fastestLocationReqInterval);
+        //mLocationRequest.setSmallestDisplacement(100);// in meters.
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        // Make sure location services are enabled.
+        // get a location settings builder and check location settings via a PendingResult
+        LocationSettingsRequest.Builder settingsBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest).setAlwaysShow(true);
+        PendingResult<LocationSettingsResult> pendingResult =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, settingsBuilder.build());
+        pendingResult.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+                Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied(ie: not in airplane mode). The client can
+                        // Make sure the app has location permissions. If it does not,
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                                PackageManager.PERMISSION_GRANTED &&
+                                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                                        PackageManager.PERMISSION_GRANTED) {
+                            // Should we show an explanation?
+                            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                                // Show an explanation to the user *asynchronously* -- don't block
+                                // this thread waiting for the user's response! After the user
+                                // sees the explanation, try again to request the permission.
+                                mLocationFragmentListener.onShowRequestPermissionRationale();
+                            } else {
+                                // No explanation needed, we can request the permission.
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                                Manifest.permission.ACCESS_COARSE_LOCATION },
+                                        PERMISSIONS_ACCESS_LOCATION_ID);
+                            }
+                            return;
+                        }
+                        // have permissions, go ahead and request updates.
+                        LocationServices.FusedLocationApi.requestLocationUpdates(
+                                mGoogleApiClient, mLocationRequest, LocationFragment.this);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    getActivity(),
+                                    REQUEST_CHECK_SETTINGS_ID);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                            e.printStackTrace();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        mLocationFragmentListener.onLocationsSettingsFailure();
+
+                }
+            }
+        });
+
+    }
+
+    // google api client has been suspended.
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    // google api client has failed.
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        try {
+            connectionResult.startResolutionForResult(getActivity(), CONN_FAIL_RESLN_RSLT_ID);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // location has been updated.
+    @Override
+    public void onLocationChanged(Location location) {
+        if (mLocationFragmentListener != null){
+            mLocationFragmentListener.onLocationUpdate(location);
+        }
+    }
+
+    public interface LocationFragmentListener {
+        void onLocationsSettingsFailure();
+        void onShowRequestPermissionRationale();
+        void onLocationUpdate(Location location);
+    }
+}
