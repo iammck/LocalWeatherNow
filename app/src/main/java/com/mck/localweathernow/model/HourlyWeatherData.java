c68840c42faac53afcc5f16f8329ace3c49914f6
Michael King <michaeldigit@gmail.com>
2016-07-02 11:47:35 -0700

    rename

--- app/src/main/java/com/mck/localweathernow/model/HourlyWeatherData.java
+++ app/src/main/java/com/mck/localweathernow/model/HourlyWeatherData.java
@@ -1,9 +1,10 @@
 package com.mck.localweathernow.model;
 
 /**
+ * container for Hourly weather data
  * Created by Michael on 5/28/2016.
  */
-public class WeatherNowData {
+public class HourlyWeatherData {
     public CurrentWeatherData currentWeatherData;
     public ForecastWeatherData forecastWeatherData;
 }
